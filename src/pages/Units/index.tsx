73e013888626a46056c4b56fa045efae66de22f9
Hariharandurairangasamy <harishyuvi2124@gmail.com>
2023-03-03 22:08:01 +0530

    SupplierData page Api Integration completed

--- src/pages/Units/index.tsx
+++ src/pages/Units/index.tsx
@@ -17,7 +17,7 @@ function Units() {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: '_id', headerName: 'ID', width: 90 },
     {
       field: 'units',
       headerName: 'Units',
@@ -62,15 +62,15 @@ function Units() {
   ]
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { _id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { _id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { _id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { _id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { _id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { _id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { _id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { _id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { _id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   ]
   const formik = useFormik({
     initialValues: {
